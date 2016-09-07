6ff2d65fc7f762501ec0f095524f2d34a71121c0
Ander Uraga Real <ander.uraga@gmail.com>
2016-09-07 10:48:27 +0200

    Realizado Test, falta implementar

--- src/main/java/com/ipartek/formacion/model/dao/UsuarioDAOSpring.java
+++ src/main/java/com/ipartek/formacion/model/dao/UsuarioDAOSpring.java
@@ -1,11 +1,17 @@
 package com.ipartek.formacion.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 import com.ipartek.formacion.model.mapper.UsuarioMapper;
 import com.ipartek.formacion.pojo.Usuario;
@@ -19,19 +25,52 @@ public class UsuarioDAOSpring implements UsuarioDAO {
 	public void setDataSource(DataSource ds) {
 		this.dataSource = ds;
 		this.jdbcTemplateObject = new JdbcTemplate(this.dataSource);
-
 	}
 
 	@Override
-	public boolean save(Usuario u) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean save(final Usuario u) {
+		boolean resul = false;
+		int affectedRows = -1;
+
+		// insert
+		if (-1 == u.getId()) {
+
+			final KeyHolder keyHolder = new GeneratedKeyHolder();
+			final String sqlInsert = "INSERT INTO `usuario` ( `nombre`, `password`, `email`) VALUES ( ? , ? , ? );";
+			affectedRows = this.jdbcTemplateObject.update(new PreparedStatementCreator() {
+				@Override
+				public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+					final PreparedStatement ps = conn.prepareStatement(sqlInsert);
+					ps.setString(1, u.getNombre());
+					ps.setString(2, u.getPassword());
+					ps.setString(3, u.getEmail());
+					return ps;
+				}
+			}, keyHolder);
+
+			u.setId(keyHolder.getKey().longValue());
+
+			// modificar
+		} else {
+			final String sqlInsert = "UPDATE `usuario` SET `nombre`=?, `password`=?, `email`=? WHERE  `id`= ?;";
+			final Object[] args = { u.getNombre(), u.getPassword(), u.getEmail(), u.getId() };
+			affectedRows = this.jdbcTemplateObject.update(sqlInsert, args);
+		}
+
+		if (affectedRows == 1) {
+			resul = true;
+		}
+		return resul;
 	}
 
 	@Override
 	public boolean borrar(long id) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean resul = false;
+		final String sql = "DELETE FROM `usuario` WHERE  `id`= ?;";
+		if (1 == this.jdbcTemplateObject.update(sql, id)) {
+			resul = true;
+		}
+		return resul;
 	}
 
 	@Override
@@ -49,7 +88,7 @@ public class UsuarioDAOSpring implements UsuarioDAO {
 	@Override
 	public Usuario checkLogin(String nombre, String pass) {
 		Usuario resul;
-		final String SQL = "select * from Usuario where nombre = ? and password = ?";
+		final String SQL = "select id, nombre, email, password from Usuario where nombre = ? and password = ?";
 		final Object[] args = new Object[] { nombre, pass };
 		try {
 			resul = this.jdbcTemplateObject.queryForObject(SQL, args, new UsuarioMapper());
--- src/test/java/com/ipartek/formacion/model/dao/UsuarioDAOSpringTest.java
+++ src/test/java/com/ipartek/formacion/model/dao/UsuarioDAOSpringTest.java
@@ -1,8 +1,18 @@
 package com.ipartek.formacion.model.dao;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -11,11 +21,82 @@ import com.ipartek.formacion.pojo.Usuario;
 
 public class UsuarioDAOSpringTest {
 
+	static ApplicationContext context;
+	static UsuarioDAOSpring daoUsuario;
+	static Usuario usuario;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		context = new ClassPathXmlApplicationContext("beans.xml");
+		daoUsuario = (UsuarioDAOSpring) context.getBean("UsuarioDAOSpring");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		context = null;
+		daoUsuario = null;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		usuario = new Usuario();
+		usuario.setEmail("eliminar@dummy.com");
+		usuario.setNombre("eliminar");
+		usuario.setPassword("123456");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		usuario = null;
+	}
+
+	@Test
+	public void save() {
+
+		// insertar usuario
+		assertTrue("Usuario No insertado" + usuario, daoUsuario.save(usuario));
+		final long id = usuario.getId();
+		assertNotEquals("Deberia tener ID <> -1 ", -1, id);
+
+		// modificar usuario
+		usuario.setEmail("borrar@borrar.com");
+		usuario.setNombre("borrar");
+		usuario.setPassword("12345456");
+		assertTrue("Usuario No Modificado" + usuario, daoUsuario.save(usuario));
+		assertEquals("Deberia ser mismo ID", id, usuario.getId());
+
+		assertTrue("No se puedo eliminar " + usuario, daoUsuario.borrar(id));
+
+	}
+
+	@Test
+	public void borrar() {
+		assertFalse("No se puedo eliminar Usuario Inexistente ", daoUsuario.borrar(-1));
+	}
+
+	@Test
+	public void buscarPorId() {
+		daoUsuario.save(usuario);
+		final long id = usuario.getId();
+		assertNotNull(daoUsuario.buscarPorId(id));
+		daoUsuario.borrar(id);
+	}
+
+	@Test
+	public void listar() {
+
+		ArrayList<Usuario> lista = (ArrayList<Usuario>) daoUsuario.listar();
+		final int numUsuarioInciales = lista.size();
+
+		daoUsuario.save(usuario);
+		lista = (ArrayList<Usuario>) daoUsuario.listar();
+
+		assertEquals((numUsuarioInciales + 1), lista.size());
+
+	}
+
 	@Test
 	public void testCheckLogin() {
-
-		final ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		final UsuarioDAOSpring daoUsuario = (UsuarioDAOSpring) context.getBean("UsuarioDAOSpring");
 
 		final Usuario uExsitente = daoUsuario.checkLogin("admin", "admin");
 		assertNotNull("Este Usuario de existir", uExsitente);
